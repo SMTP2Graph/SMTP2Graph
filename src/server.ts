57ad3212ce35c00d1c82a653acf85bce32936ea9
RoelVB <roelvb@smtp2graph.com>
2024-02-04 16:20:53 +0100

    Added version (-v/---version) parameter

--- src/server.ts
+++ src/server.ts
@@ -3,21 +3,26 @@ import { log } from './classes/Logger';
 import { MailQueue } from './classes/MailQueue';
 import { SMTPServer } from './classes/SMTPServer';
 
-(async ()=>{
-    // Validate the config before continuing
-    try {
-        Config.validate();
-    } catch(error) {
-        await log('error', `Invalid config. ${String(error)}`, {error});
-        process.exit(1);
-    }
+if(process.argv.includes('-v') || process.argv.includes('--version')) // We're asked for our version?
+    console.log(`SMTP2Graph v${VERSION}`);
+else
+{
+    (async ()=>{
+        // Validate the config before continuing
+        try {
+            Config.validate();
+        } catch(error) {
+            await log('error', `Invalid config. ${String(error)}`, {error});
+            process.exit(1);
+        }
 
-    const queue = new MailQueue();
-    const server = new SMTPServer(queue);
-    try {
-        await server.listen();
-    } catch(error) {
-        log('error', `Failed to start SMTP server. ${String(error)}`, {error});
-        process.exit(1);
-    }
-})();
+        const queue = new MailQueue();
+        const server = new SMTPServer(queue);
+        try {
+            await server.listen();
+        } catch(error) {
+            log('error', `Failed to start SMTP server. ${String(error)}`, {error});
+            process.exit(1);
+        }
+    })();
+}
