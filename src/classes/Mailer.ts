8f0001001213449f03e6aa1faab40f36736d0e08
Roel <10999128+RoelVB@users.noreply.github.com>
2026-03-09 14:38:08 +0100

    Undo unnecessary changes and add typing

--- src/classes/Mailer.ts
+++ src/classes/Mailer.ts
@@ -5,42 +5,44 @@ import axios, { AxiosRequestConfig, AxiosResponse, isAxiosError } from 'axios';
 import { Base64Encode } from 'base64-stream';
 import addressparser from 'nodemailer/lib/addressparser';
 import { ConfidentialClientApplication } from '@azure/msal-node';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 import { Config } from './Config';
 import { UnrecoverableError } from './Constants';
 import { MsalProxy } from './MsalProxy';
-import { HttpsProxyAgent } from 'https-proxy-agent';
 
 export class MailboxAccessDenied extends UnrecoverableError { }
 export class InvalidMailContent extends UnrecoverableError { }
 export class MessageSizeExceeded extends UnrecoverableError { }
 
-export class Mailer {
+export class Mailer
+{
     /** Prevent getting an accesstoken in parallel */
     static #aquireTokenMutex = new Mutex();
     /** Prevent sending more than 4 messages in parallel (see: https://learn.microsoft.com/en-us/graph/throttling-limits#outlook-service-limits) */
     static #sendSemaphore = new Semaphore(4);
 
-    static #msalClient = (Config.clientId && (Config.clientSecret || (Config.clientCertificateThumbprint && Config.clientCertificateKeyPath))) ? new ConfidentialClientApplication({
+    static #msalClient = (Config.clientId && (Config.clientSecret || (Config.clientCertificateThumbprint && Config.clientCertificateKeyPath)))?new ConfidentialClientApplication({
         auth: {
             authority: Config.msalAuthority,
             clientId: Config.clientId,
             clientSecret: Config.clientSecret,
-            clientCertificate: Config.clientCertificateThumbprint && Config.clientCertificateKeyPath ? {
+            clientCertificate: Config.clientCertificateThumbprint && Config.clientCertificateKeyPath?{
                 thumbprint: Config.clientCertificateThumbprint,
                 privateKey: Config.clientCertificateKey!,
-            } : undefined,
+            }:undefined,
         },
-        system: Config.httpProxyConfig ? { networkClient: new MsalProxy() } : undefined, // We use our custom implementation, because the `proxyUrl` property doesn't want to work
-    }) : undefined;
+        system: Config.httpProxyConfig?{networkClient: new MsalProxy()}:undefined, // We use our custom implementation, because the `proxyUrl` property doesn't want to work
+    }):undefined;
 
-    static async sendEml(filePath: string) {
-        return this.#sendSemaphore.runExclusive(async () => {
+    static async sendEml(filePath: string)
+    {
+        return this.#sendSemaphore.runExclusive(async ()=>{
             // Determine the sender
             let sender = Config.forceMailbox;
-            if (!sender) // There's no forced sender in the config, so we get it from the mail data
+            if(!sender) // There's no forced sender in the config, so we get it from the mail data
             {
                 const senderObj = await this.#findSender(filePath);
-                if (!senderObj) throw new UnrecoverableError('No sender/from address defined');
+                if(!senderObj) throw new UnrecoverableError('No sender/from address defined');
                 sender = senderObj.address;
             }
 
@@ -50,8 +52,9 @@ export class Mailer {
             // Send the message
             const readStream = fs.createReadStream(filePath);
             try {
-                let proxyAgent;
-                if (Config.httpProxyConfig) {
+                let proxyAgent: HttpsProxyAgent | undefined;
+                if(Config.httpProxyConfig)
+                {
                     const { protocol, host, port, auth } = Config.httpProxyConfig;
                     const authPart = auth ? `${encodeURIComponent(auth.username)}:${encodeURIComponent(auth.password)}@` : '';
                     proxyAgent = new HttpsProxyAgent(`${protocol}://${authPart}${host}:${port}`);
@@ -70,15 +73,17 @@ export class Mailer {
                     httpAgent: proxyAgent,
                     proxy: false,
                 });
-            } catch (error: any) {
-                if (isAxiosError(error) && error.response?.data) {
+            } catch(error: any) {
+                if(isAxiosError(error) && error.response?.data)
+                {
                     const data = error.response?.data;
-                    if ('error' in data && 'code' in data.error) {
-                        if (data.error.code === 'ErrorAccessDenied')
+                    if('error' in data && 'code' in data.error)
+                    {
+                        if(data.error.code === 'ErrorAccessDenied')
                             throw new MailboxAccessDenied(`Access to mailbox "${sender}" denied`);
-                        else if (data.error.code === 'ErrorMimeContentInvalidBase64String')
+                        else if(data.error.code === 'ErrorMimeContentInvalidBase64String')
                             throw new InvalidMailContent(`Invalid content for mail "${filePath}"`);
-                        else if (data.error.code === 'ErrorMessageSizeExceeded')
+                        else if(data.error.code === 'ErrorMessageSizeExceeded')
                             throw new MessageSizeExceeded(`The message exceeds the maximum supported size for mail "${filePath}"`);
                         else
                             throw new Error(JSON.stringify(data.error));
@@ -95,31 +100,33 @@ export class Mailer {
     }
 
     /** Automatically retry a request when it's being throttled by the Graph API */
-    static async #retryableRequest<RequestData = any, ReponseData = any>(request: AxiosRequestConfig<RequestData>): Promise<AxiosResponse<RequestData, ReponseData>> {
+    static async #retryableRequest<RequestData = any, ReponseData = any>(request: AxiosRequestConfig<RequestData>): Promise<AxiosResponse<RequestData, ReponseData>>
+    {
         const retryLimit = 3;
         let retryCount = 0;
         let wait = 200;
 
-        const retry = async (): Promise<AxiosResponse<RequestData, ReponseData>> => {
+        const retry = async (): Promise<AxiosResponse<RequestData, ReponseData>> =>
+        {
             const abortController = new AbortController();
-            const connectTimeout = setTimeout(() => abortController.abort(`Server did not respond within 10 seconds`), 10000);
-            const overallTimeout = setTimeout(() => abortController.abort(`Failed to send message within 120 seconds`), 120000);
+            const connectTimeout = setTimeout(()=>abortController.abort(`Server did not respond within 10 seconds`), 10000);
+            const overallTimeout = setTimeout(()=>abortController.abort(`Failed to send message within 120 seconds`), 120000);
 
             try {
                 return await axios({
                     ...request,
                     signal: abortController.signal,
-                    onUploadProgress: progress => {
+                    onUploadProgress: progress=>{
                         clearTimeout(connectTimeout);
                     },
                 });
-            } catch (error) {
-                if (++retryCount > retryLimit) // We've reached our retry limit?
+            } catch(error) {
+                if(++retryCount > retryLimit) // We've reached our retry limit?
                     throw error;
-                else if (isAxiosError(error) && (error.response?.status === 429 || error.response?.status === 503 || error.response?.status === 504)) // We got a retryable response?
+                else if(isAxiosError(error) && (error.response?.status === 429 || error.response?.status === 503 || error.response?.status === 504)) // We got a retryable response?
                 {
                     const retryAfter = error.response.headers['Retry-After'];
-                    if (retryAfter && !isNaN(retryAfter)) // We got throttled
+                    if(retryAfter && !isNaN(retryAfter)) // We got throttled
                         wait = parseInt(retryAfter) * 1000;
                     else
                         wait *= 2;
@@ -128,7 +135,7 @@ export class Mailer {
 
                     return retry();
                 }
-                else if (axios.isCancel(error) && abortController.signal.aborted)
+                else if(axios.isCancel(error) && abortController.signal.aborted)
                     throw abortController.signal.reason;
                 else // Unknown error response, throw the error
                     throw error;
@@ -141,25 +148,28 @@ export class Mailer {
         return retry();
     }
 
-    static #sleep(ms: number): Promise<void> {
-        return new Promise(r => setTimeout(r, ms));
+    static #sleep(ms: number): Promise<void>
+    {
+        return new Promise(r=>setTimeout(r, ms));
     }
 
     /** Get sender address from EML/RFC822 data */
-    static async #findSender(filePath: string) {
+    static async #findSender(filePath: string)
+    {
         const readStream = fs.createReadStream(filePath);
         const reader = readline.createInterface({
             input: readStream,
             crlfDelay: Infinity, // To treat \r\n and \n the same
         });
 
-        for await (const line of reader) {
-            if (line === '') // We've reached the end of the headers?
+        for await(const line of reader)
+        {
+            if(line === '') // We've reached the end of the headers?
                 break;
-            else if (line.toLowerCase().startsWith('sender:') || line.toLowerCase().startsWith('from:')) // Found the sender?
+            else if(line.toLowerCase().startsWith('sender:') || line.toLowerCase().startsWith('from:')) // Found the sender?
             {
-                const parsed = addressparser(line.substring(line.indexOf(':') + 1), { flatten: true });
-                if (parsed.length && parsed[0].address) // We got an address?
+                const parsed = addressparser(line.substring(line.indexOf(':')+1), {flatten: true});
+                if(parsed.length && parsed[0].address) // We got an address?
                 {
                     readStream.destroy();
                     return parsed[0];
@@ -170,9 +180,10 @@ export class Mailer {
         readStream.destroy();
     }
 
-    static async #aquireToken(): Promise<string> {
-        return this.#aquireTokenMutex.runExclusive(async () => {
-            if (!this.#msalClient) throw new UnrecoverableError('Trying to login without an application registration');
+    static async #aquireToken(): Promise<string>
+    {
+        return this.#aquireTokenMutex.runExclusive(async ()=>{
+            if(!this.#msalClient) throw new UnrecoverableError('Trying to login without an application registration');
 
             const res = await this.#msalClient.acquireTokenByClientCredential({
                 scopes: ['https://graph.microsoft.com/.default'],
