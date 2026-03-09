7693f5e8c79fd69b8f107554bef0cb8d59cd3830
Roel <10999128+RoelVB@users.noreply.github.com>
2026-03-09 14:41:57 +0100

    Undid some more unnecessary changes and added typing

--- src/classes/MsalProxy.ts
+++ src/classes/MsalProxy.ts
@@ -1,19 +1,22 @@
 import { INetworkModule, NetworkRequestOptions, NetworkResponse } from "@azure/msal-node";
+import { HttpsProxyAgent } from 'https-proxy-agent';
 import axios, { AxiosRequestConfig } from "axios";
 import { Config } from "./Config";
-import { HttpsProxyAgent } from 'https-proxy-agent';
 
-export class MsalProxy implements INetworkModule {
-    sendGetRequestAsync<T>(url: string, options?: NetworkRequestOptions): Promise<NetworkResponse<T>> {
+export class MsalProxy implements INetworkModule
+{
+    sendGetRequestAsync<T>(url: string, options?: NetworkRequestOptions): Promise<NetworkResponse<T>>
+    {
         return this.#sendRequestAsync(url, 'GET', options);
     }
 
-    sendPostRequestAsync<T>(url: string, options?: NetworkRequestOptions): Promise<NetworkResponse<T>> {
+    sendPostRequestAsync<T>(url: string, options?: NetworkRequestOptions): Promise<NetworkResponse<T>>
+    {
         return this.#sendRequestAsync(url, 'POST', options);
     }
 
     async #sendRequestAsync<T>(url: string, method: 'GET' | 'POST', options: NetworkRequestOptions = {}): Promise<NetworkResponse<T>> {
-        let proxyAgent;
+        let proxyAgent: HttpsProxyAgent | undefined;
         if (Config.httpProxyConfig) {
             const { protocol, host, port, auth } = Config.httpProxyConfig;
             const authPart = auth ? `${encodeURIComponent(auth.username)}:${encodeURIComponent(auth.password)}@` : '';
